99874f4d20d565a2ce944bf2572596b243dc7f8a
You <you@macdeMac-mini.local>
2026-04-16 21:51:28 +0800

    tavily tool

--- main.py
+++ main.py
@@ -3,34 +3,21 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()  # Load environment variables from .env file
 
-tavily = TavilyClient()
 
-@tool
-def search_web(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching the web for: {query}")
-    result = tavily.search(query=query)
-    return result
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-tools = [search_web]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke({"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in Singapore on linkedin and list their details")})
     print(f"Agent's response: {result}")
 
 
